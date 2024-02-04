9c6960b155ac152774f5a17b87c39a0f40fd2709
mohamad zoubi <m.zoubi2@hotmail.com>
2024-02-05 02:05:23 +0300

    added scroll event

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,23 @@
 import twitter from "../images/socials/twitter.png";
 import discord from "../images/socials/discord.png";
+import { useState } from "react";
 
 function Navbar(){
 
+    const [navBg, setNavBg] = useState(false);
+
+    const handleScroll = () => {
+        if (window.scrollY > 150) {
+            setNavBg(true);
+        } else {
+            setNavBg(false);
+        }
+      };
+
+      window.addEventListener("scroll", handleScroll);
 
     return(
-        <div className="navbar">
+        <div className={navBg ? "navbar nav-bg" : "navbar"} >
             <div className="nav-container">
                 <h3>CRYPTOWAVE</h3>
                 <ul className="nav-links">
